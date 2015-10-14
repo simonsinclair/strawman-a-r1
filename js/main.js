3cc6ee2beb6654560414f990babf44990f481176
Simon Sinclair <simon@sinclair>
2015-10-14 16:22:46 +0100

    Implement/and clean up various state tracking

--- js/main.js
+++ js/main.js
@@ -6,29 +6,30 @@
 
   var A = {
 
-    following:  [],
+    following: [],
 
     // INITIALIZE
     //
 
     init: function() {
-      A.coldStart();
       A.bindEvts();
-      A.updateBodyClasses();
+      A.updateBodyRegisteredOrNot();
+      A.updateBodyLoggedInOrNot();
+      A.updateBodyFollowedTopics();
+    },
 
-      // Test
-      //
-
+    testAll: function() {
       A.register();
       A.login();
       A.follow('mars');
       A.follow('david-beckham');
+      A.unFollow('mars');
     },
 
     coldStart: function() {
       A.following  = [];
 
-      Cookies.remove('logged_in');
+      Cookies.remove('logged-in');
       Cookies.remove('registered');
       Cookies.remove('following', []);
     },
@@ -50,18 +51,18 @@
     },
 
     onRegistered: function() {
-      A.updateBodyClasses();
+      A.updateBodyRegisteredOrNot();
     },
 
     // - login
 
     login: function() {
-      Cookies.set('logged_in', true);
+      Cookies.set('logged-in', true);
       $.publish('loggedIn');
     },
 
     onLoggedIn: function() {
-      A.updateBodyClasses();
+      A.updateBodyLoggedInOrNot();
     },
 
     // FOLLOWING
@@ -74,7 +75,7 @@
     onFollow: function(e, topic) {
       A.following.push( topic );
       Cookies.set('following', A.following);
-      A.updateBodyClasses();
+      A.updateBodyFollowedTopics();
     },
 
     unFollow: function(topic) {
@@ -82,38 +83,89 @@
     },
 
     onUnFollowed: function(e, topic) {
-      // Remove 'topic' from following array
-      // Update 'following' cookie value
+      var topicToRemove = topic;
+      var index         = A.following.indexOf( topicToRemove );
+      if (index > -1) {
+        A.following.splice( index, 1 );
+      }
+
+      Cookies.set('following', A.following);
+      A.updateBodyFollowedTopics( topicToRemove );
     },
 
     // UPDATE
     //
 
-    updateBodyClasses: function() {
-      var bodyClasses = '';
+    updateBodyRegisteredOrNot: function() {
+      $('body').removeClass('registered anonymous');
 
-      // Registered?
       if( Cookies.get('registered') ) {
-        bodyClasses += ' registered';
+        $('body').addClass('registered');
+      } else {
+        $('body').addClass('anonymous');
+      }
+    },
+
+    updateBodyLoggedInOrNot: function() {
+      $('body').removeClass('logged-in logged-out');
+
+      if( Cookies.get('logged-in') ) {
+        $('body').addClass('logged-in');
+      } else {
+        $('body').addClass('logged-out');
+      }
+    },
+
+
+    // The below method puts the names of the topics followed in the body class
+    //
+
+    updateBodyFollowedTopics: function(topicToRemove) {
+      var following = Cookies.getJSON('following') || [];
+
+      // If there is a topic to remove, that's all we need to do.
+      if(topicToRemove) {
+
+        // Remove unfollowed topic.
+        $('body').removeClass( topicToRemove );
+
+      } else {
+
+        // Add followed topics using cookie array.
+        $.each(following, function(i, topic) {
+          $('body').addClass( topic );
+        });
+
       }
 
-      // Logged-in?
-      if( Cookies.get('logged_in') ) {
-        bodyClasses += ' logged-in';
+      A.updateBodyFollowingOrNot();
+    },
+
+
+    // The below method updates the body classes with 'following' or 'not-following'
+    //
+
+    updateBodyFollowingOrNot: function() {
+      var following   = Cookies.getJSON('following') || [];
+      var isFollowing = following.length > 0 ? true : false;
+
+      // First remove both following and not-following classes, so we can...
+      $('body').removeClass('following not-following');
+
+      // ...add either or, below.
+      if(isFollowing) {
+        $('body').addClass('following');
+      } else {
+        $('body').addClass('not-following');
       }
-
-      // What/Who're you following?
-      $.each(A.following, function(i, topic) {
-        bodyClasses += ' '+topic;
-      });
-
-      $('body').addClass( bodyClasses );
-    }
+    },
 
   };
 
   // Expose
   w.A = A;
 
+  // Initialise
   $(A.init);
+
 })(this, jQuery);
