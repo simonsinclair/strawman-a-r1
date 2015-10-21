80b2d2702136dc5d88c99d94e85a55826a51cd55
Simon Sinclair <simon@sinclair>
2015-10-21 11:20:56 +0100

    Implement ?reset=true query string to cold start

--- js/main.js
+++ js/main.js
@@ -187,6 +187,13 @@
         $clickedMenuItem.toggleClass('expanded');
 
       }
+    },
+
+    getParameterByName: function(name) {
+      name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+      var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+      return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
     }
 
   };
